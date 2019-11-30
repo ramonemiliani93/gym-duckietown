03b72924c6022fcc7cdf68cff0855b9e5f411b25
Mostafa Elaraby <mostafa.elaraby@tensorgraph.io>
2019-11-29 22:31:21 -0500

    experimenting a new model

--- learning/imitation/uncertainty_models/montecarlo_squeezenet.py
+++ learning/imitation/uncertainty_models/montecarlo_squeezenet.py
@@ -20,12 +20,23 @@ class MonteCarloSqueezenet(nn.Module):
         
         self.model = models.squeezenet1_1(pretrained=True)
         # removing some high level features not needed in this context
-        self.model.features = nn.Sequential(*list(self.model.features.children())[:6])
-        final_conv = nn.Conv2d(32, self.num_outputs, kernel_size=1, stride=1)
-        self.model.classifier = nn.Sequential(
+        self.model.features = nn.Sequential(*list(self.model.features.children())[:9])
+        # we have 256 output channels
+        self.velocity_branch = nn.Sequential(
             nn.Dropout(p=self.p),
-            nn.Conv2d(128, 64, kernel_size=3, stride=1),
+            nn.Conv2d(256, 128, kernel_size=3, stride=1),
             nn.ReLU(inplace=True),
+            nn.Conv2d(128, 32, kernel_size=3, stride=1)
+        )
+
+        self.omega_branch = nn.Sequential(
+            nn.Dropout(p=self.p),
+            nn.Conv2d(256, 128, kernel_size=3, stride=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(128, 32, kernel_size=3, stride=1)
+        )
+        final_conv = nn.Conv2d(32, self.num_outputs, kernel_size=1, stride=1)
+        self.classifier = nn.Sequential(
             nn.Conv2d(64, 32, kernel_size=3, stride=1),
             nn.Dropout(p=self.p),
             final_conv,
@@ -33,24 +44,29 @@ class MonteCarloSqueezenet(nn.Module):
         )
         self.model.num_classes = self.num_outputs
         self.episode = 0
-        self.n_epochs = 0
-        self.fixed_velocity = 0.65 #TODO add to params space
+        self.n_epochs = 0 
         self.freeze_pretrained_modules()
-        #self.criterion = SmoothL1Loss(reduction='mean')
+        self.init_weights(final_conv)
+        
 
-        for m in self.model.classifier.modules():
-            if isinstance(m, nn.Conv2d):
-                if m is final_conv:
-                    init.normal_(m.weight, mean=0.0, std=0.01)
-                else:
-                    init.kaiming_uniform_(m.weight)
-                if m.bias is not None:
-                    init.constant_(m.bias, 0)
-
-        #TODO add auto tune paramter for the first episode
+    def init_weights(self, final_conv):
+        for subbranch in [self.velocity_branch, self.omega_branch, self.classifier]:
+            for m in subbranch.modules():
+                if isinstance(m, nn.Conv2d):
+                    if m is final_conv:
+                        init.normal_(m.weight, mean=0.0, std=0.01)
+                    else:
+                        init.kaiming_uniform_(m.weight)
+                    if m.bias is not None:
+                        init.constant_(m.bias, 0)
 
     def forward(self, images):
-        output = self.model(images)
+        features_extracted = self.model.features(images) 
+        v_feats = self.velocity_branch(features_extracted) 
+        omega_feats = self.omega_branch(features_extracted)
+        # late_fusion
+        feats = torch.cat((v_feats, omega_feats),1)
+        output = self.classifier(feats)
         return output
     
     def freeze_pretrained_modules(self):
@@ -72,13 +88,15 @@ class MonteCarloSqueezenet(nn.Module):
         if self.num_outputs==1:
             loss = F.mse_loss(prediction,target[:,-1].reshape(-1,1), reduction='mean')
         else:
+            # custom loss predicting classification class for speed and regression value for omega
+            loss_v = prediction[:,0]
             loss = F.mse_loss(prediction,target, reduction='mean')
         return loss
     
 
     def predict(self, *args):
         images = args[0]
-        output = self.model(images)
+        output = self.forward(images)
         if self.num_outputs==1:
             # in case of only predicting omega
             v_tensor = torch.tensor([self.fixed_velocity],dtype = output.dtype).to(self._device)
