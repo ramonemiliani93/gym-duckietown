ea9e5130e48fb4752213bd0dc88c99ceae0ee804
Mostafa Elaraby <mostafa.elaraby@tensorgraph.io>
2019-11-29 19:46:49 -0500

    updated dagger mix to take into account current bot position

--- learning/imitation/algorithms/dagger.py
+++ learning/imitation/algorithms/dagger.py
@@ -10,12 +10,31 @@ class DAgger(InteractiveImitationLearning):
         # expert decay
         self.p = alpha
         self.alpha = self.p
+        self.convergence_distance = 0.1
+        self.convergence_angle = np.pi / 12 
+        self.angle_limit = np.pi / 6 
+        self.distance_limit = 0.2
 
     def _mix(self):
         control_policy = np.random.choice(
             a=[self.teacher, self.learner],
             p=[self.alpha, 1. - self.alpha]
         )
+        try:
+            lp = self.environment.get_lane_pos2(self.environment.cur_pos, self.environment.cur_angle)
+        except :
+            # this means we are need the teacher definitely
+            return self.teacher
+        if self.active_policy:
+            # check for convergence if we are using the teacher to move back to our learner
+            if abs(lp.dist) < self.convergence_distance or abs(lp.angle_rad) < self.convergence_angle:
+                return self.learner
+        else:
+            # in case we are using our learner and it started to diverge a lot we need to give 
+            # control back to expert 
+            if abs(lp.dist)>self.distance_limit or abs(lp.angle_rad)> self.angle_limit:
+                return self.teacher
+
 
         return control_policy
 
