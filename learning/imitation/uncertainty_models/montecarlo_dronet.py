482ca2fbbfef556c5c36b87d99a714ca40869956
Mostafa Elaraby <mostafa.elaraby@tensorgraph.io>
2019-12-03 09:15:16 -0500

    speed threshold bug fix

--- learning/imitation/uncertainty_models/montecarlo_dronet.py
+++ learning/imitation/uncertainty_models/montecarlo_dronet.py
@@ -74,10 +74,12 @@ class MonteCarloDronet(nn.Module):
         self.col_corner =nn.Sequential(
             nn.Linear(self.num_feats_extracted, 1)
         )
+        self.max_speed = 0.38
+        self.min_speed = 0.18
 
-        self.max_speed = torch.tensor(0.38).to(self._device)
-        self.min_speed = torch.tensor(0.18).to(self._device)
-        self.speed_threshold = 0.5
+        self.max_speed_tensor = torch.tensor(self.max_speed).to(self._device)
+        self.min_speed_tensor = torch.tensor(self.min_speed).to(self._device)
+        self.speed_threshold = (self.max_speed + self.min_speed) / 2
         self.decay = 1/10
         self.alpha = 0
         self.epoch_0 = 10
@@ -107,7 +109,7 @@ class MonteCarloDronet(nn.Module):
         prob_corner, omega = self.forward(images)
         # post processing v values to its max and min counterparts
         prob_corner = torch.sigmoid(prob_corner) 
-        v_tensor = v_tensor = torch.where(prob_corner>0.5, self.max_speed, self.min_speed )
+        v_tensor = v_tensor = torch.where(prob_corner>0.5, self.max_speed_tensor, self.min_speed_tensor )
         output = torch.cat((v_tensor, omega), 1)
         return output
 
