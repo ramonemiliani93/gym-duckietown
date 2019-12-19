bug fixes and updated way of scaling the angle

learning/imitation/learners/neural_network_policy.py
@@ -110,7 +110,7 @@ class NeuralNetworkPolicy(BaseLearner):
         try:
             # scaling velocity to become in 0-1 range which is multiplied by max speed to get actual vel
             # also scaling steering angle to become in range -1 to 1 to make it easier to regress
-            expert_actions = [np.array([expert_action[0], np.tanh(expert_action[1] )  ]) for expert_action in expert_actions]
+            expert_actions = [np.array([expert_action[0], expert_action[1] / (np.pi / 2)  ]) for expert_action in expert_actions]
         except:
             # at the start the teacher is sending just [[0]]
             pass

learning/imitation/uncertainty_models/montecarlo_dronet.py
@@ -85,6 +85,7 @@ class MonteCarloDronet(nn.Module):
         self.max_velocity = max_velocity
         self.max_speed_tensor = torch.tensor(self.max_velocity).to(self._device)
         self.min_speed_pure_pursuit = (self.max_velocity) * 0.5
+        self.min_speed_tensor = torch.tensor(self.min_speed_pure_pursuit).to(self._device)
 
     def forward(self, images):
         features = self.feature_extractor(images)
@@ -108,7 +109,7 @@ class MonteCarloDronet(nn.Module):
         images = args[0]
         is_speed_up, steering_angle = self.forward(images)
         is_speed_up = torch.sigmoid(is_speed_up)
-        v_tensor  =  torch.where(is_speed_up>0.5, self.max_speed_tensor, self.min_speed_pure_pursuit )  # (is_speed_up) * self.max_speed_tensor + (1 - is_speed_up) * self.min_speed_pure_pursuit  # 
+        v_tensor  =  torch.where(is_speed_up>0.5, self.max_speed_tensor, self.min_speed_tensor )  # (is_speed_up) * self.max_speed_tensor + (1 - is_speed_up) * self.min_speed_pure_pursuit  # 
         steering_angle =  steering_angle * ( np.pi / 2)
         output = torch.cat((v_tensor, steering_angle), 1)
         return output