7523717666667c8e9574b582b0657e5102e8a321
Mostafa Elaraby <mostafa.elaraby@tensorgraph.io>
2019-12-19 12:49:19 -0500

    updated when to start map randomization

--- learning/imitation/algorithms/iil_learning.py
+++ learning/imitation/algorithms/iil_learning.py
@@ -124,7 +124,7 @@ class InteractiveImitationLearning:
         self._episode_done_listeners.append(listener)
 
     def _on_episode_done(self):
-        if self._episode > 7:
+        if self._episode > 5:
             self.environment.randomize_maps_on_reset = True
         else:
             self.randomize_maps_on_reset = False
